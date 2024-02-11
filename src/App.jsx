68f530f5a0d1b5a2cbffe556139c4fa6c362e697
node <ratafak15@gmail.com>
2024-02-11 18:25:39 -0500

    Disenos y estructura aun sigue

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,48 @@
+import CrossIcon from "./components/icons/CrossIcon";
+
 const App =() => {
   return (
-    <div className="bg-[url('./assets/bg-mobile-light.jpg')] bg-no-repeat bg-contain ">
+    <div className="bg-[url('./assets/bg-mobile-light.jpg')] bg-no-repeat bg-contain bg-gray-300 min-h-screen">
 <header className="container mx-auto px-4 pt-8">
 <div className="flex justify-between">
 <h1 className="uppercase text-white text-2xl font-semibold tracking-[0.3em]">ToD0</h1>
    <button>Luna</button>
 </div>
-   <form className="bg-white rounded-md overflow-hidden py-4">
-    <span className="rounded-full border-2 inline-block h-3 w-3"></span>
-    <input type="text" placeholder="Create a new todo..."/>
+   <form className="bg-white rounded-md overflow-hidden py-4 flex gap-4 items-center px-4 mt-7">
+    <span className="rounded-full border-2 inline-block h-5 w-5"></span>
+    <input type="text" placeholder="Create a new todo..." className="w-full text-gray-500 outline-none"/>
    </form>
 </header>
-<main className="container mx-auto px-4">
-  <article>
-   <button>Circulo</button>
-   <p>Complete</p>
-   <button>X</button>
+<main className="container mx-auto px-4 mt-8">
+  <div className="bg-white rounded-md px-4">
+  <article className="flex gap-4 py-4">
+   <button className="rounded-full border-2 inline-block h-5 w-5"></button>
+   <p className="text-gray-600 grow">Complete</p>
+   <button className="flex-none">
+    <CrossIcon />
+   </button>
   </article>
   <article>
-   <button>Circulo</button>
-   <p>Complete</p>
-   <button>X</button>
+   <button className="rounded-full border-2 inline-block h-5 w-5"></button>
+   <p className="text-gray-600">Complete</p>
+   <button>
+    <CrossIcon />
+   </button>
   </article>
   <article>
-   <button>Circulo</button>
-   <p>Complete</p>
-   <button>X</button>
+   <button className="rounded-full border-2 inline-block h-5 w-5"></button>
+   <p className="text-gray-600">Complete</p>
+   <button>
+    <CrossIcon />
+   </button>
   </article>
+  
 
   <section>
     <span>5 Cosas por hacer</span>
     <button>Limpiar</button>
   </section>
+  </div>
 </main>
 <section>
   <button className="container mx-auto px-4">Todo</button>
@@ -54,5 +65,6 @@ const App =() => {
 export default App;
 /* Es un archivo para modificar mucho del VSC */ 
 
+// ml- auto para empujar elementos hacia completamente a la esquina
 
 // Con Control  y espacio al estar erscribiendo dentro de una clase se pueden invocar funcionalidades con tailwinds
