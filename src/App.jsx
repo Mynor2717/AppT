4ed46e3f9933c7d2dc4f9a3576c8bada344ee776
node <ratafak15@gmail.com>
2024-02-12 14:11:37 -0500

    Diseno terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import CrossIcon from "./components/icons/CrossIcon";
+import Luna from "./components/icons/Luna";
 
 const App =() => {
   return (
@@ -6,7 +7,9 @@ const App =() => {
 <header className="container mx-auto px-4 pt-8">
 <div className="flex justify-between">
 <h1 className="uppercase text-white text-2xl font-semibold tracking-[0.3em]">ToD0</h1>
-   <button>Luna</button>
+   <button>
+    <Luna className="fill-red-400"/>
+   </button>
 </div>
    <form className="bg-white rounded-md overflow-hidden py-4 flex gap-4 items-center px-4 mt-7">
     <span className="rounded-full border-2 inline-block h-5 w-5"></span>
@@ -14,45 +17,46 @@ const App =() => {
    </form>
 </header>
 <main className="container mx-auto px-4 mt-8">
-  <div className="bg-white rounded-md px-4">
-  <article className="flex gap-4 py-4">
+  <div className="bg-white rounded-md">
+  <article className="flex gap-4 py-4 border-b-gray-400 border-b-2 px-4">
    <button className="rounded-full border-2 inline-block h-5 w-5"></button>
    <p className="text-gray-600 grow">Complete</p>
    <button className="flex-none">
     <CrossIcon />
    </button>
   </article>
-  <article>
+  <article className="flex gap-4 py-4 border-b-gray-400 border-b-2 px-4">
    <button className="rounded-full border-2 inline-block h-5 w-5"></button>
-   <p className="text-gray-600">Complete</p>
-   <button>
+   <p className="text-gray-600 grow">Complete</p>
+   <button className="flex-none">
     <CrossIcon />
    </button>
   </article>
-  <article>
+  <article className="flex gap-4 py-4 border-b-gray-400 border-b-2 px-4">
    <button className="rounded-full border-2 inline-block h-5 w-5"></button>
-   <p className="text-gray-600">Complete</p>
-   <button>
+   <p className="text-gray-600 grow">Complete</p>
+   <button className="flex-none">
     <CrossIcon />
    </button>
   </article>
-  
-
-  <section>
-    <span>5 Cosas por hacer</span>
-    <button>Limpiar</button>
+ 
+  <section className="py-4 px-4 flex justify-between">
+    <span className="text-gray-400">5 Cosas por hacer</span>
+    <button className="text-gray-400">Limpiar</button>
   </section>
   </div>
 </main>
-<section>
-  <button className="container mx-auto px-4">Todo</button>
-  <button>Activar</button>
-  <button>Completar</button>
 
+<section className="container mx-auto px-4 mt-8">
+  <div className="bg-white flex justify-center rounded-md p-4 gap-4">
+  <button className="text-blue-600">Todo</button>
+  <button className="hover:text-blue-600">Activar</button>
+  <button className="hover:text-blue-600">Completar</button>
+  </div>
 </section>
 
 <section>
-  <p className="text-center">Drag and Drop</p>
+  <p className="text-center mt-8">Drag and Drop</p>
 </section>
 
   </div>
