54050651b21eb82e0bcac00df0221c0dfa425ace
node <ratafak15@gmail.com>
2024-02-17 20:34:33 -0500

    Dark mode & sun Icon

--- src/components/icons/Header.jsx
+++ src/components/icons/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <div className="flex justify-between">
                 <h1 className="uppercase text-white text-2xl font-semibold tracking-[0.3em]">ToD0</h1>
                 <button>
-                    <Luna className="fill-red-400" />
+                    <Luna  />
                 </button>
             </div>
 
